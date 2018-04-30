649e1df0d0dad783d6ae3661cacb22c7cba1343f
Martin Craig <martin.craig@eng.ox.ac.uk>
2018-04-30 15:15:48 +0100

    Add calib into toplevel package

--- oxasl/__init__.py
+++ oxasl/__init__.py
@@ -5,7 +5,8 @@ ASL Python tools
 from ._version import __version__
 
 from .image import AslImage, AslOptionGroup, AslWorkspace
+from .calib import calib
 from . import basil
 from . import fslwrap as fsl
 
-__all__ = ["AslImage", "AslWorkspace", "AslOptionGroup", "basil", "fsl"]
+__all__ = ["AslImage", "AslWorkspace", "AslOptionGroup", "basil", "fsl", "calib"]
