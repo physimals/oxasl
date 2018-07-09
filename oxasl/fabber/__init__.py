fbec01213b8a5b08dad92dc41de69a6abf1effea
Martin Craig <martin.craig@eng.ox.ac.uk>
2018-07-09 13:11:18 +0100

    Fabber API moved to its own module (pyfab)

--- oxasl/fabber/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Python API for the FSL Fabber tool
-"""
-
-from .api import Fabber, FabberException, FabberRun, percent_progress
-from .model_test import self_test, generate_test_data
-
-__all__ = ["Fabber", "FabberException", "FabberRun", "self_test", "generate_test_data", "percent_progress"]
