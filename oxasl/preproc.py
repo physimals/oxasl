e2a9e0e43d5d432e2cef8c58f5aed66d250b0570
Martin Craig <martin.craig@eng.ox.ac.uk>
2018-07-09 13:07:05 +0100

    Refactoring of options into a category

--- oxasl/preproc.py
+++ oxasl/preproc.py
@@ -2,24 +2,47 @@
 ASL data preprocessing command line tool
 """
 import sys
-from optparse import OptionParser
 
 import scipy
 
 from fsl.wrappers import mcflirt, LOAD
 
-from .image import AslImage, add_data_options, AslOptionGroup
+from .options import AslOptionParser, OptionCategory, IgnorableOptionGroup
+from .image import AslImage, AslImageOptions
 
-def add_preproc_options(parser, ignore=()):
-    g = AslOptionGroup(parser, "Preprocessing", ignore=ignore)
-    g.add_option("--diff", dest="diff", help="Perform tag-control subtraction", action="store_true", default=False)
-    g.add_option("--smooth", dest="smooth", help="Spatially smooth data", action="store_true", default=False)
-    g.add_option("--fwhm", dest="fwhm", help="FWHM for spatial filter kernel", type="float", default=6)
-    g.add_option("--mc", dest="mc", help="Motion correct data", action="store_true", default=False)
-    g.add_option("--reorder", dest="reorder", help="Re-order data in specified order")
-    parser.add_option_group(g)
+class AslPreprocOptions(OptionCategory):
+    """
+    OptionCategory which contains options for preprocessing ASL data
+    """
 
-def preprocess(asldata, diff=False, reorder=None, mc=False, smooth=False, fwhm=None, ref=None, log=sys.stdout, **kwargs):
+    def __init__(self, **kwargs):
+        OptionCategory.__init__(self, "preproc", **kwargs)
+
+    def groups(self, parser):
+        group = IgnorableOptionGroup(parser, "Preprocessing", ignore=self.ignore)
+        group.add_option("--diff", help="Perform tag-control subtraction", action="store_true", default=False)
+        group.add_option("--smooth", help="Spatially smooth data", action="store_true", default=False)
+        group.add_option("--fwhm", help="FWHM for spatial filter kernel", type="float", default=6)
+        group.add_option("--mc", help="Motion correct data", action="store_true", default=False)
+        group.add_option("--ref", help="Optional reference image for motion correction", default=None)
+        group.add_option("--reorder", help="Re-order data in specified order")
+        return [group, ]
+
+def preprocess(asldata, diff=False, reorder=None, mc=False, smooth=False, fwhm=None, ref=None, log=sys.stdout):
+    """
+    Basic preprocessing of ASL data
+
+    :param asldata: AslImage instance
+    :param diff: If True, perform tag/control subtraction
+    :param reorder: If specified, reorder the data using this ordering string
+    :param mc: If True, perform motion correction using mcflirt
+    :param smooth: If True, perform smoothing
+    :param fwhm: If smooth=True, the full-width-half-maximum for the smoothing kernel
+    :param ref: If mc=True, a reference image for motion correction (default is to use the middle volume)
+    :param log: Output log stream (default: stdout)
+
+    :return: AslImage instance
+    """
     log.write("ASL preprocessing...\n")
 
     # Keep original AslImage with info about TIs, repeats, etc
@@ -37,12 +60,12 @@ def preprocess(asldata, diff=False, reorder=None, mc=False, smooth=False, fwhm=N
 
     if mc: 
         log.write("  - Motion correction\n")
-        output = mcflirt(asldata, cost="mutualinfo", out=LOAD)
-        asldata = asldata.derived(output["out"].nibImage.get_data(), suffix="_mc")
+        mcimg = mcflirt(asldata, cost="mutualinfo", out=LOAD, reffile=ref)["out"]
+        asldata = orig.derived(mcimg.data, suffix="_mc")
 
     if smooth:
         sigma = round(fwhm/2.355, 2)
-        log.write("Spatial smoothing with FWHM: %f (sigma=%f)\n" % (fwhm, sigma))
+        log.write("  - Spatial smoothing with FWHM: %f (sigma=%f)\n" % (fwhm, sigma))
         smoothed = scipy.ndimage.gaussian_filter(asldata.nibImage.get_data(), sigma=sigma)
         asldata = asldata.derived(smoothed, suffix="_smooth")
 
@@ -54,27 +77,32 @@ def main():
     Entry point for command line tool
     """
     try:
-        p = OptionParser(usage="asl_preproc -i <filename> [options]", version="@VERSION@")
-        add_data_options(p, output_type="file")
-        add_preproc_options(p)
-        options, _ = p.parse_args(sys.argv)
+        parser = AslOptionParser(usage="asl_preproc -i <filename> [options]")
+        parser.add_option("-o", dest="output", help="Output file", default=None)
+        parser.add_option("--debug", dest="debug", help="Debug mode", action="store_true", default=False)
+        
+        parser.add_category(AslImageOptions())
+        parser.add_category(AslPreprocOptions())
+
+        options, _ = parser.parse_args(sys.argv)
+        kwopts = vars(options)
 
         if not options.asldata:
             sys.stderr.write("Input file not specified\n")
-            p.print_help()
+            parser.print_help()
             sys.exit(1)
 
-        options.asldata = AslImage(options.asldata, role="Input data", **vars(options))
+        options.asldata = AslImage(options.asldata, role="Input data", **parser.filter(kwopts, "image"))
         if options.output is None:
             options.output = options.asldata.name + "_out"
         options.asldata.summary()
         print("")
 
-        data_preproc = preprocess(**vars(options))
+        data_preproc = preprocess(options.asldata, **parser.filter(kwopts, "preproc"))
         data_preproc.save(options.output)
 
-    except Exception as e:
-        sys.stderr.write("ERROR: " + str(e) + "\n")
+    except ValueError as exc:
+        sys.stderr.write("ERROR: " + str(exc) + "\n")
         sys.exit(1)
 
 if __name__ == "__main__":
