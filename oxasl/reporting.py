0bee1f4ba7588e5798aa7be2cf5334ebbebef5d2
Martin Craig <martin.craig@eng.ox.ac.uk>
2018-08-14 11:09:42 +0100

    Allow the addition of sub-reports

--- oxasl/reporting.py
+++ oxasl/reporting.py
@@ -187,6 +187,7 @@ class Report(object):
         self._files = {}
         self._start_time = datetime.datetime.now()
         self._end_time = None
+        self.extension = ""
 
     def generate_html(self, dest_dir, build_dir=None):
         """
@@ -195,27 +196,21 @@ class Report(object):
         self._end_time = datetime.datetime.now()
 
         if build_dir:
-            if not os.path.exists(build_dir):
-                os.makedirs(build_dir)
-            elif not os.path.isdir(build_dir):
-                raise ValueError("Report build directory %s exists but is not a directory" % build_dir)
-            else:
-                warnings.warn("Report build directory %s already exists" % build_dir)
+            if os.path.exists(build_dir):
+                if not os.path.isdir(build_dir):
+                    raise ValueError("Report build directory %s exists but is not a directory" % build_dir)
+                else:
+                    warnings.warn("Report build directory %s already exists" % build_dir)
             is_temp = False
         else:
             build_dir = tempfile.mkdtemp("_report")
             is_temp = True
 
         try:
+            self.tofile(build_dir)
+
             with open(os.path.join(build_dir, "conf.py"), "w") as conffile:
                 conffile.write(REPORT_CONF)
-
-            with open(os.path.join(build_dir, "index.rst"), "w") as indexfile:
-                rst_files = "\n".join(["  %s" % rst_file for rst_file in self._contents])
-                indexfile.write(INDEX_TEMPLATE % (self._start_time.strftime("%Y-%m-%d %H:%M:%S"), self._end_time.strftime("%Y-%m-%d %H:%M:%S"), rst_files))
-
-            for fname, content in self._files.items():
-                content.tofile(os.path.join(build_dir, fname))
 
             os.system('sphinx-build -M html "%s" "%s"' % (build_dir, os.path.join(build_dir, "_build")))
 
@@ -230,6 +225,20 @@ class Report(object):
         finally:
             if is_temp:
                 shutil.rmtree(build_dir)
+
+    def tofile(self, build_dir):
+        if not os.path.exists(build_dir):
+            os.makedirs(build_dir)
+
+        with open(os.path.join(build_dir, "index.rst"), "w") as indexfile:
+            rst_files = "\n".join(["  %s" % rst_file for rst_file in self._contents])
+            start_time = self._start_time.strftime("%Y-%m-%d %H:%M:%S")
+            if self._end_time: end_time = self._end_time.strftime("%Y-%m-%d %H:%M:%S")
+            else: end_time = ""
+            indexfile.write(INDEX_TEMPLATE % (start_time, end_time, rst_files))
+
+        for fname, content in self._files.items():
+            content.tofile(os.path.join(build_dir, fname))
 
     def add(self, name, content, overwrite=False):
         """
@@ -247,6 +256,8 @@ class Report(object):
         self._files[fname] = content
         if isinstance(content, ReportPage):
             self._contents.append(name)
+        if isinstance(content, Report):
+            self._contents.append(name + "/index")
 
 REPORT_CONF = """
 # This file is execfile()d with the current directory set to its
