ada5bb1a777376f161477fc328dcbfd326824d8a
Martin Craig <martin.craig@eng.ox.ac.uk>
2018-10-09 14:32:42 +0100

    Cleanup options and split preproc/modelling steps

--- oxasl/oxford_asl.py
+++ oxasl/oxford_asl.py
@@ -1,7 +1,7 @@
 #!/bin/env python
 """
-OXFORD_ASL: Converts ASL images into perfusion maps
-===================================================
+OXASL: Converts ASL images into perfusion maps
+==============================================
 
 Michael Chappell, FMRIB Image Analysis Group & IBME
 
@@ -55,13 +55,12 @@ from __future__ import print_function
 import sys
 import os
 import traceback
-import collections
 
 import numpy as np
 
 from fsl.data.image import Image
 
-from oxasl import Workspace, AslImage, __version__, image, calib, struc, basil, preproc, mask, corrections, reg
+from oxasl import Workspace, __version__, image, calib, struc, basil, mask, corrections, reg
 from oxasl.options import AslOptionParser, GenericOptions, OptionCategory, IgnorableOptionGroup
 
 class OxfordAslOptions(OptionCategory):
@@ -70,7 +69,7 @@ class OxfordAslOptions(OptionCategory):
     """
 
     def __init__(self, **kwargs):
-        OptionCategory.__init__(self, "oxford_asl", **kwargs)
+        OptionCategory.__init__(self, "oxasl", **kwargs)
 
     def groups(self, parser):
         ret = []
@@ -78,12 +77,20 @@ class OxfordAslOptions(OptionCategory):
         g.add_option("--wp", dest="wp", help="Analysis which conforms to the 'white papers' (Alsop et al 2014)", action="store_true", default=False)
         g.add_option("--mc", dest="mc", help="Motion correct data", action="store_true", default=False)
         g.add_option("--fixbat", dest="inferbat", help="Fix bolus arrival time", action="store_false", default=True)
+        g.add_option("--fixbolus", dest="infertau", help="Fix bolus duration", action="store_false", default=True)
         g.add_option("--artoff", dest="inferart", help="Do not infer arterial component", action="store_false", default=True)
         ret.append(g)
         g = IgnorableOptionGroup(parser, "Acquisition/Data specific")
         g.add_option("--bat", dest="bat", help="Bolus arrival time (default=0.7 (pASL), 1.3 (cASL)", type=float)
         g.add_option("--t1", dest="t1", help="Tissue T1 value", type=float, default=1.3)
         g.add_option("--t1b", dest="t1b", help="Blood T1 value", type=float, default=1.65)
+        ret.append(g)
+        g = IgnorableOptionGroup(parser, "Output options")
+        g.add_option("--save-corrected", help="Save corrected input data", action="store_true", default=False)
+        g.add_option("--save-reg", help="Save registration information (transforms etc)", action="store_true", default=False)
+        g.add_option("--save-basil", help="Save Basil modelling output", action="store_true", default=False)
+        g.add_option("--save-calib", help="Save calibration output", action="store_true", default=False)
+        g.add_option("--save-all", help="Save all output (enabled when --debug specified)", action="store_true", default=False)
         ret.append(g)
         return ret
 
@@ -93,7 +100,7 @@ def main():
     """
     debug = True
     try:
-        parser = AslOptionParser(usage="oxford_asl -i <asl_image> [options]", version=__version__)
+        parser = AslOptionParser(usage="oxasl -i <asl_image> [options]", version=__version__)
         parser.add_category(image.AslImageOptions())
         parser.add_category(struc.StructuralImageOptions())
         parser.add_category(OxfordAslOptions())
@@ -105,27 +112,20 @@ def main():
         if not options.output:
             options.output = "oxasl"
 
-        if not options.asldata:
-            sys.stderr.write("Input file not specified\n")
-            parser.print_help()
-            sys.exit(1)
-
-        # Some oxford_asl command-line specific defaults
+        # Some oxasl command-line specific defaults
         options.spatial = True
         if options.calib is not None and options.calib_method is None:
             if options.struc is not None:
                 options.calib_method = "refregion"
             else:
                 options.calib_method = "voxelwise"
+        if options.debug:
+            options.save_all = True
 
-        wsp = Workspace(savedir=options.output, **vars(options))
-        wsp.asldata = AslImage(options.asldata, **parser.filter(vars(wsp), "image"))
+        wsp = Workspace(savedir=options.output, separate_input=True, auto_asldata=True, **vars(options))
+        #wsp.asldata = AslImage(options.asldata, **parser.filter(vars(options), "image"))
 
         oxasl(wsp)
-        report_build_dir = None
-        if wsp.debug:
-            report_build_dir = os.path.join(options.output, "report_build")
-        wsp.report.generate_html(os.path.join(options.output, "report"), report_build_dir)
 
     except Exception as e:
         sys.stderr.write("ERROR: " + str(e) + "\n")
@@ -134,40 +134,41 @@ def main():
         sys.exit(1)
 
 def oxasl(wsp):
+    wsp.log.write("OXASL - running\n")
+    wsp.log.write("Version: %s\n" % __version__)
+    wsp.log.write("Input ASL data: %s\n" % wsp.asldata.name)
+    wsp.asldata.summary(wsp.log)
+
+    oxasl_preproc(wsp)
+    oxasl_model(wsp)
+
+    wsp.log.write("\nOutput is %s\n" % wsp.savedir)
+    wsp.log.write("OXASL - done\n")
+
+def oxasl_preproc(wsp):
     """
     Run standard processing on ASL data
 
     This method requires wsp to be a Workspace containing certain standard information.
     As a minimum, the attribute ``asldata`` must contain an AslImage object.
     """
- 
-    wsp.log.write("OXFORD_ASL - running\n")
-    wsp.log.write("Version: %s\n" % __version__)
-    wsp.log.write("Input ASL data: %s\n" % wsp.asldata.name)
-    wsp.asldata.summary(wsp.log)
-
-    # Always output in native space
-    wsp.output_spaces = ["native", ]
-
-    calib.init(wsp)
     struc.init(wsp)
-    preproc.init(wsp)
+    corrections.apply_corrections(wsp)
 
-    if wsp.mc: 
-        corrections.get_motion_correction(wsp)
+    corrections.get_motion_correction(wsp)
     corrections.apply_corrections(wsp)
 
     reg.reg_asl2calib(wsp)
     reg.reg_asl2struc(wsp, True, False)
 
-    if wsp.fmap:
-        corrections.get_fieldmap_correction(wsp)
-    if wsp.cblip:
-        corrections.get_cblip_correction(wsp)
+    corrections.get_fieldmap_correction(wsp)
+    corrections.get_cblip_correction(wsp)
+    corrections.get_sensitivity_correction(wsp)
     corrections.apply_corrections(wsp)
 
     mask.generate_mask(wsp)
 
+def oxasl_model(wsp):
     if wsp.asldata.ntis > 1:
         # For multi TI/PLD data, set a more liberal prior for tissue ATT since we should be able to 
         # determine this from the data. NB this leaves the arterial BAT alone.
@@ -178,7 +179,7 @@ def oxasl(wsp):
     # Re-do ASL->structural registration using BBR and perfusion image
     if wsp.structural.struc is not None:
         wsp.reg.regfrom_orig = wsp.reg.regfrom
-        new_regfrom = wsp.basil.main.finalstep.mean_ftiss.data
+        new_regfrom = np.copy(wsp.basil.finalstep.mean_ftiss.data)
         new_regfrom[new_regfrom < 0] = 0
         wsp.reg.regfrom = Image(new_regfrom, header=wsp.reg.regfrom.header)
         wsp.reg.asl2struc_initial = wsp.reg.asl2struc
@@ -187,9 +188,10 @@ def oxasl(wsp):
         reg.reg_asl2struc(wsp, False, True)
 
     # We could at this point re-apply corrections derived from structural space
-
     if wsp.pvcorr:
-        if wsp.struc is not None:
+        # Partial volume correction is very sensitive to the mask, so recreate it
+        # if it came from the structural image
+        if wsp.mask is None and wsp.struc is not None:
             # If mask was generated from structural image update it from new registration
             wsp.rois.mask_orig = wsp.rois.mask
             wsp.done("generate_mask", status=False)
@@ -203,36 +205,70 @@ def oxasl(wsp):
         basil.basil(wsp, output_wsp=wsp.sub("basil_pvcorr"), prefit=False)
 
     do_output(wsp)
-    if not wsp.debug:
-        cleanup(wsp)
+    do_report(wsp)
+    do_cleanup(wsp)
 
-    wsp.log.write("\nOutput is %s\n" % wsp.savedir)
-    wsp.log.write("OXFORD_ASL - done\n")
+def do_report(wsp):
+    """
+    Generate HTML report
+    """
+    if not wsp.savedir:
+        return
+    report_build_dir = None
+    if wsp.debug:
+        report_build_dir = os.path.join(wsp.savedir, "report_build")
+    wsp.log.write("\nGenerating HTML report\n")
+    report_dir = os.path.join(wsp.savedir, "report")
+    wsp.report.generate_html(report_dir, report_build_dir)
+    wsp.log.write(" - Report generated in %s\n" % report_dir)
 
 def do_output(wsp):
-    for space in wsp.output_spaces:
+    """
+    Create output images
+    """
+    calib.init(wsp)
+    # Always output in native space
+    output_spaces = ["native", ]
+    output_items = {
+        "ftiss" : ("perfusion", 6000, True),
+        "fblood" : ("aCBV", 100, True),
+        "delttiss" : ("arrival", 1, False),
+        "ftisswm" : ("perfusion_wm", 6000, True),
+        "deltwm" : ("arrival_wm", 1, False),
+        "modelfit" : ("modelfit", 1, False),
+    }
+    for space in output_spaces:
         output_wsp = wsp.sub(space)
-        # Make negative values = 0
-        img = wsp.basil.main.finalstep.mean_ftiss
-        img.data[img.data < 0] = 0
-        output_wsp.perfusion = img
-        if wsp.calibration.calib is not None:
-            output_wsp.perfusion_calib = calib.calibrate(wsp, output_wsp.perfusion)
+        for fabber_output, oxasl_output in output_items.items():
+            img = wsp.basil.finalstep.ifnone("mean_%s" % fabber_output, None)
+            if img is not None:
+                # Make negative values = 0 and ensure masked value zeroed
+                data = np.copy(img.data)
+                data[img.data < 0] = 0
+                data[wsp.rois.mask.data == 0] = 0
+                img = Image(data, header=img.header)
+                name, multiplier, is_perfusion = oxasl_output
+                if is_perfusion:
+                    img, = corrections.apply_sensitivity_correction(wsp, img)
+                setattr(output_wsp, name, img)
+                if is_perfusion and wsp.calib is not None:
+                    wsp.multiplier = multiplier
+                    setattr(output_wsp, "%s_calib" % name, calib.calibrate(wsp, img))
 
-def _deletable(value):
-    return isinstance(value, (Image, np.ndarray)) or isinstance(value, collections.Sequence) or isinstance(value, collections.Mapping) 
-
-def cleanup(wsp):
-    output_spaces = wsp.output_spaces
-    if output_spaces is None: output_spaces = []
-    for attr in dir(wsp):
-        value = getattr(wsp, attr)
-        if not attr.startswith("__") and attr not in output_spaces:
-            if isinstance(value, Workspace):
-                print("Deleting workspace %s" % attr)
-                cleanup(value)
-            elif _deletable(value):
-                print("Deleting %s" % attr)
-                delattr(wsp, attr)
-            else:
-                print("Keeping %s (%s)" % (attr, value))
+def do_cleanup(wsp):
+    """
+    Remove items from the workspace that are not being output. The
+    corresponding files will be deleted
+    """
+    if not (wsp.save_all or wsp.save_corrected):
+        wsp.corrected = None
+        wsp.distcorr = None
+        wsp.moco = None
+        wsp.topup = None
+        wsp.fieldmap = None
+    if not (wsp.save_all or wsp.save_reg):
+        wsp.reg = None
+    if not (wsp.save_all or wsp.save_basil):
+        wsp.basil = None
+    if not (wsp.save_all or wsp.save_calib):
+        wsp.calibration = None
