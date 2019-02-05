3a18037029efc1f62681442a5e80b98db6266dc9
Martin Craig <martin.craig@eng.ox.ac.uk>
2019-02-05 11:58:47 +0000

    Updated for pip and pypi release

--- setup.py
+++ setup.py
@@ -23,11 +23,11 @@ def git_version():
         # Full version includes the Git commit hash
         full_version = subprocess.check_output('git describe --dirty', shell=True).decode("utf-8").strip(" \n")
 
-        # Python standardized version in form major.minor.patch.dev<build>
+        # Python standardized version in form major.minor.patch.post<build>
         version_regex = re.compile(r"v?(\d+\.\d+\.\d+(-\d+)?).*")
         match = version_regex.match(full_version)
         if match:
-            std_version = match.group(1).replace("-", ".dev")
+            std_version = match.group(1).replace("-", ".post")
         else:
             raise RuntimeError("Failed to parse version string %s" % full_version)
         return full_version, std_version
@@ -63,7 +63,7 @@ def get_version(rootdir):
         update_metadata(rootdir, version, timestamp)
     else:
         # Could not get metadata from Git - use the version file if it exists
-        with open(os.path.join(rootdir, MODULE, '_version.py'), encoding='utf-8') as f:
+        with io.open(os.path.join(rootdir, MODULE, '_version.py'), encoding='utf-8') as f:
             md = f.read()
             match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", md, re.M)
             if match:
