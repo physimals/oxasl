3d353e4c5a4073525bd7bad0fde3933fd715aa80
Martin Craig <martin.craig@nottingham.ac.uk>
2020-10-13 11:41:19 +0100

    Save mask in ASL and structural space and remove unused command line entry point

--- oxasl/mask.py
+++ oxasl/mask.py
@@ -3,16 +3,13 @@ OXASL - Module to generate a suitable mask for ASL data
 
 Copyright (c) 2008-2020 Univerisity of Oxford
 """
-import sys
-
 import numpy as np
 import scipy as sp
 
 import fsl.wrappers as fsl
 from fsl.data.image import Image
 
-from oxasl import __version__, AslImage, Workspace, image, reg, struc
-from oxasl.options import AslOptionParser, GenericOptions
+from oxasl import reg
 from oxasl.reporting import LightboxImage
 
 def run(wsp):
@@ -48,6 +45,7 @@ def run(wsp):
         return
 
     wsp.sub("rois")
+    wsp.log.write("\nGenerating ASL data mask\n")
 
     # Reporting
     page = wsp.report.page("mask")
@@ -62,8 +60,9 @@ def run(wsp):
         wsp.rois.mask_src = "struc"
         page.heading("Brain extracted structural image", level=1)
         page.image("struc_brain", LightboxImage(wsp.structural.brain, bgimage=wsp.structural.struc))
-        brain_mask_asl = reg.change_space(wsp, wsp.structural.brain_mask, "native")
-        wsp.rois.mask = Image(sp.ndimage.morphology.binary_fill_holes((brain_mask_asl.data > 0.25)).astype(np.int), header=brain_mask_asl.header)
+        wsp.rois.mask_struc = wsp.structural.brain_mask
+        wsp.rois.mask_asl = reg.change_space(wsp, wsp.structural.brain_mask, "native")
+        wsp.rois.mask = Image(sp.ndimage.morphology.binary_fill_holes((wsp.rois.mask_asl.data > 0.25)).astype(np.int), header=wsp.rois.mask_asl.header)
         mask_source = "generated from brain extracting structural image and registering to ASL space"
     else:
         # Alternatively, use registration image (which will be BETed calibration or mean ASL image)
@@ -71,7 +70,6 @@ def run(wsp):
         wsp.rois.mask = Image((wsp.reg.nativeref.data != 0).astype(np.int), header=wsp.reg.nativeref.header)
         mask_source = "generated from brain extracted registration ASL image"
 
-    wsp.log.write("\nGenerated ASL data mask\n")
     wsp.log.write(" - Mask %s\n" % mask_source)
 
     page.heading("Masked ASL brain image", level=1)
@@ -82,40 +80,3 @@ def run(wsp):
         page.image("mask_outline", LightboxImage(wsp.rois.mask, bgimage=wsp.asldata.perf_weighted(), outline=True))
     else:
         page.image("mask_outline", LightboxImage(wsp.rois.mask, bgimage=wsp.asldata.mean(), outline=True))
-
-def main():
-    """
-    Entry point for command line tool
-    """
-    try:
-        parser = AslOptionParser(usage="asl_mask -i <asl_image> [options...]", version=__version__)
-        parser.add_option("--calib", "-c", help="Calibration image", default=None)
-        parser.add_option("--use-pwi", help="Use the perfusion weighted average rather than the timeseries mean", action="store_true", default=False)
-        parser.add_category(image.AslImageOptions())
-        parser.add_category(struc.StructuralImageOptions())
-        parser.add_category(GenericOptions())
-
-        options, _ = parser.parse_args(sys.argv)
-        options.mask = None # No point in using command line tool if you already have a mask!
-        wsp = Workspace(**vars(options))
-
-        if not options.asldata:
-            sys.stderr.write("Input file not specified\n")
-            parser.print_help()
-            sys.exit(1)
-
-        wsp.asldata = AslImage(wsp.asldata, **parser.filter(vars(options), "image"))
-        wsp.asldata.summary()
-
-        wsp.generate_mask()
-
-        if wsp.output is None:
-            wsp.output = wsp.asldata.name + "_mask"
-        wsp.rois.mask.save(wsp.output)
-
-    except ValueError as exc:
-        sys.stderr.write("ERROR: " + str(exc) + "\n")
-        sys.exit(1)
-
-if __name__ == "__main__":
-    main()
