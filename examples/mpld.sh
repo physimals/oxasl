1d1215be73f56f2866d33e296e724aae6db634b7
Martin Craig <martin.craig@eng.ox.ac.uk>
2021-07-05 20:56:43 +0100

    Add example scripts using FSL course data

---
+++ examples/mpld.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+DATADIR=/Users/ctsu0221/data/asl/fsl_course/
+#DEBUG=--debug
+
+oxasl -i ${DATADIR}/mpld_asltc --casl --iaf=tc --ibf=tis --slicedt=0.0452 \
+      --tis=1.65,1.9,2.15,2.4,2.65,2.9 --bolus=1.4 \
+      --fslanat=${DATADIR}/T1.anat --senscorr \
+      -c ${DATADIR}/aslcalib --tr=4.8 --cmethod=single --csf=${DATADIR}/csfmask \
+      --cblip=${DATADIR}/aslcalib_PA --echospacing=0.00952 --pedir=y \
+      --mc \
+      -o mpld_out --overwrite ${DEBUG}
+
+
---
+++ examples/run_all.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sh mpld.sh
+sh mpld_voxelwise.sh
+sh mpld_pvc.sh
+sh mpld_region_analysis.sh
